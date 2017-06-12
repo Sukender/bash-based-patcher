09eb57eea12589ed3156017a71c6bb632e9fed4f
Sukender <sukender@virtuelcity.com>
2017-06-12 23:14:02 +0200

    Added basic options handling, added version/author/license info, added exit codes, added variables, added (untested) patch downloader.

--- diff.sh
+++ diff.sh
@@ -1,26 +1,128 @@
 #!/bin/bash
 # Creates a compressed binary diff (patch) between two directories.
+# Author: Sukender (Benoit Neil)
+# Licence: WTFPL v0
+# Version: 0.2
+
 # This uses :
 #   - 'tar' to make the directory a single stream (file)
 #   - 'rdiff' to make the binary diff
 #   - named pipes to avoid storing intermediate files to disk (much faster: everything in memory).
 #   - 7-zip (7za) to LZMA compress the output and make an efficient patch.
 
+# Error codes
+# 0: Ok
+# 1: Error in arguments
+# 2: Error in process
+
+toolName="Sukender's rdiff-based patcher"
+separatorDisplay="--------------------------------------------------------------------------------"
+
+defaultpatchfile="patch.7z"
+patchfile="$defaultpatchfile"
+verbose=0
+
+# --------------------------------------------------------------------------------
+# Parameters parsing and usage
+
+positionalOption() {
+	if [ -z "$dir1" ]; then
+		dir1="$1"
+	else
+		if [ -z "$dir2" ]; then
+			dir2="$1"
+		else
+			echo "Unrecognized option: $1"
+			exit 1
+		fi
+	fi
+}
+
+usage() {
+	echo "Creates a compressed binary diff (patch) between two directories."
+	echo
+	echo "Syntax:"
+	echo "  diff [options] oldDirectory newDirectory"
+	echo
+	echo "Options:"
+	echo
+	echo "  -h, --help"
+	echo "      This help text."
+	echo "  -p, --patch NAME"
+	echo "      Changes the output patch file name to NAME (default: '$defaultpatchfile')."
+	echo "      File is always overwritten without confirmation."
+	echo "  -v, --verbose"
+	echo "      Adds more information."
+	echo "  --"
+	echo "      Do not interpret any more arguments as options."
+	echo
+}
+
+
+while [ "$#" -gt 0 ]; do
+case "$1" in
+	-v|--verbose)
+		shift; verbose=1;
+		;;
+	-h|--help)
+		shift; usage; exit 0;
+		;;
+	-p|--patch)
+		shift; patchfile="$1"; shift;
+		;;
+	--)
+		shift;
+		break;
+		;;
+	# an option argument, continue
+	*)
+		positionalOption "$1"
+		shift;
+		;;
+esac
+done
+
+if [ -z "$dir1" ] || [ -z "$dir2" ]; then
+	usage;
+	exit 1;
+fi
+
+if [[ $verbose != 0 ]]; then
+	echo "$toolName"
+	echo $separatorDisplay
+	echo ""
+	echo "Generating patch from '$dir1' to '$dir2'"
+	echo "Patch file: $patchfile"
+fi
+
+# --------------------------------------------------------------------------------
+
+echo $separatorDisplay
 echo "[$(date +%H:%M:%S)] Creating patch. This may take a while."
 echo "    Please note this is a multiple phases creation: the output compressor may stall (especially at 0%) for a long time."
-
-# dir1="$(cygpath "1")"
-# dir2="$(cygpath "2")"
-# dir1="$(cygpath "D:\Prog\VStory-Maquette\VStory Vierge - Unity 5.5.1 - VStory 4.3.0.0 - 64bits")"
-# dir2="$(cygpath "D:\Prog\VStory-Maquette\VStory Vierge - Unity 5.5.2 - VStory 5.0.0.0 - 64bits")"
-dir1="$1"
-dir2="$2"
-patchfile="patch.7z"
+echo ""
 
 rm pipe1 pipe2 "$patchfile" 2> /dev/null
-mkfifo pipe1 pipe1sig pipe2
-tar -c --sort=name --no-auto-compress --directory="$dir1" . > pipe1 & rdiff signature pipe1 pipe1sig & tar -c --sort=name --no-auto-compress --directory="$dir2" . > pipe2 & rdiff delta pipe1sig pipe2 | 7za a -mx9 -si "$patchfile"
+mkfifo pipe1 pipe1sig pipe2 || exit 2
+tar -c --sort=name --no-auto-compress --directory="$dir1" . > pipe1 & rdiff signature pipe1 pipe1sig & tar -c --sort=name --no-auto-compress --directory="$dir2" . > pipe2 & rdiff delta pipe1sig pipe2 | 7za a -mx9 -si "$patchfile" || exit 2
 	# > /dev/null
 rm pipe1 pipe1sig pipe2
 
+echo $separatorDisplay
 echo "[$(date +%H:%M:%S)] Patch created."
+echo ""
+
+# --------------------------------------------------------------------------------
+
+# Insecure FTP upload:
+# HOST='transfert.virtuelcity.com'
+# USER='transfert'
+# PASSWD='???????'
+
+# ftp -n $HOST <<END_SCRIPT
+# quote USER $USER
+# quote PASS $PASSWD
+# binary
+# put $patchfile
+# quit
+# END_SCRIPT
--- patch.sh
+++ patch.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # Applies a compressed binary diff (patch) on a directory, creating a copy (ie. not in-place).
+# Author: Sukender (Benoit Neil)
+# Licence: WTFPL v0
+# Version: 0.2
+
 # This uses :
 #   - 'tar' to make the directory a single stream (file)
 #   - 'rdiff' to make the binary diff
 #   - named pipes to avoid storing intermediate files to disk (much faster: everything in memory).
 #   - 7-zip (7za) to LZMA compress the output and make an efficient patch.
+#   - wget to retreive a distant patch.
+
+# Error codes
+# 0: Ok
+# 1: Error in arguments
+# 2: Error in process
+# 3: Error in downloading patch
 
 # Theorical call would be:
 #   7za x -so "$patchFile" > pipe2ar & tar -c --sort=name --no-auto-compress --directory="$dir1" . > pipe1 & rdiff patch pipe1 pipe2ar pipe2 & tar -x --directory="$dir2" . > pipe2
@@ -15,24 +26,123 @@
 #mkfifo pipe1 pipe2 pipe2ar
 #rm pipe1 pipe2 pipe2ar
 
-echo "[$(date +%H:%M:%S)] Creating intermediate file before applying patch. This may take a while."
+toolName="Sukender's rdiff-based patcher"
+separatorDisplay="--------------------------------------------------------------------------------"
 
-# dir1="$(cygpath "1")"
-#dir2="$(cygpath "2bis")"
-dir1="$1"
-dir2="$1_patched"
-patchFile="patch.7z"
+defaultpatchfile="patch.7z"
+patchfile="$defaultpatchfile"
 intermediate1="1.tar"
+verbose=0
 
-mkdir -p "$dir2"
+# --------------------------------------------------------------------------------
+# Parameters parsing and usage
+
+positionalOption() {
+	if [ -z "$dir1" ]; then
+		dir1="$1"
+	else
+		if [ -z "$dir2" ]; then
+			dir2="$1"
+		else
+			echo "Unrecognized option: $1"
+			exit 1
+		fi
+	fi
+}
+
+usage() {
+	echo "Applies a compressed binary diff (patch) on a directory, creating a copy (ie. not in-place)."
+	echo
+	echo "Syntax:"
+	echo "  patch [options] oldDirectory [newDirectory]"
+	echo "  If newDirectory is omitted, it is automatically generated."
+	echo
+	echo "Options:"
+	echo
+	echo "  -h, --help"
+	echo "      This help text."
+	echo "  -g, --get URL"
+	echo "      Downloads URL as the patch to apply."
+	echo "  -p, --patch NAME"
+	echo "      Uses the patch file NAME (default: '$defaultpatchfile')."
+	echo "  -v, --verbose"
+	echo "      Adds more information."
+	echo "  --"
+	echo "      Do not interpret any more arguments as options."
+	echo
+}
+
+
+while [ "$#" -gt 0 ]; do
+case "$1" in
+	-v|--verbose)
+		shift; verbose=1;
+		;;
+	-h|--help)
+		shift; usage; exit 0;
+		;;
+	-p|--patch)
+		shift; patchfile="$1"; shift;
+		;;
+	-g|--get)
+		shift; patchUrl="$1"; shift;
+		;;
+	--)
+		shift;
+		break;
+		;;
+	# an option argument, continue
+	*)
+		positionalOption "$1"
+		shift;
+		;;
+esac
+done
+
+if [ -z "$dir1" ]; then
+	usage;
+	exit 1;
+fi
+
+if [ -z "$dir2" ]; then
+	dir2="$1_patched"
+fi
+
+if [[ $verbose != 0 ]]; then
+	echo "$toolName"
+	echo $separatorDisplay
+	echo ""
+	echo "Applying patch on '$dir1', to '$dir2'"
+	echo "Patch file: $patchfile"
+	echo "Patch URL: $patchUrl"
+fi
+
+# --------------------------------------------------------------------------------
+
+if [ -n "$patchUrl" ]; then
+	echo $separatorDisplay
+	echo "[$(date +%H:%M:%S)] Getting the patch file."
+	echo ""
+	wget "$patchUrl" -O "$patchfile" || exit 3
+fi
+
+echo $separatorDisplay
+echo "[$(date +%H:%M:%S)] Creating intermediate file before applying patch. This may take a while."
+echo ""
+
 rm "$intermediate1" pipe2 pipe2ar 2> /dev/null
-mkfifo pipe2 pipe2ar
-tar -cf "$intermediate1" --sort=name --no-auto-compress --directory="$dir1" .
+rm -r "$dir2" 2> /dev/null
+mkdir -p "$dir2"
+mkfifo pipe2 pipe2ar || exit 2
+tar -cf "$intermediate1" --sort=name --no-auto-compress --directory="$dir1" . || exit 2
 
+echo $separatorDisplay
 echo "[$(date +%H:%M:%S)] Applying patch. This may take a while."
+echo ""
 
-#7za x -so "$patchFile" > _testpatch
-7za x -so "$patchFile" > pipe2ar & rdiff patch "$intermediate1" pipe2ar pipe2 & tar -xf pipe2 --directory="$dir2" .
+7za x -so "$patchfile" > pipe2ar & rdiff patch "$intermediate1" pipe2ar pipe2 & tar -xf pipe2 --directory="$dir2" . || exit 2
 rm "$intermediate1" pipe2 pipe2ar
 
+echo $separatorDisplay
 echo "[$(date +%H:%M:%S)] Patch applied."
+echo ""
